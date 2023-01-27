1d741011fbae710c3b9edc54415446c9c76ed20c
Vasyl Dizhak <vasyl.dizhak@epidemicsound.com>
2023-01-27 22:04:27 +0100

    Fix, upgrade dependencies

--- client/src/App.test.js
+++ client/src/App.test.js
@@ -1,8 +1,8 @@
 import { render, screen } from '@testing-library/react';
 import App from './App';
 
-test('renders learn react link', () => {
+test('renders app with tracks tab', () => {
   render(<App />);
-  const linkElement = screen.getByText(/learn react/i);
+  const linkElement = screen.getByText(/tracks/i);
   expect(linkElement).toBeInTheDocument();
 });
