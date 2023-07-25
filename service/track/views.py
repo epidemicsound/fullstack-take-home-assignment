18326ee2ca654d6b88ba20a0a74a3adff8805610
Hamid Tehrani <hamid.tehrani@viaplaygroup.com>
2023-07-25 14:47:20 +0200

    Customized viewset to support different list params

--- service/track/views.py
+++ service/track/views.py
@@ -1,11 +1,11 @@
-from rest_framework import permissions, viewsets
+from rest_framework import permissions
 
 from core.models import Track
-
+from core.custom_model_viewset import CustomModelViewSet
 from track.serializers import TrackSerializer
 
 
-class TrackViewSet(viewsets.ModelViewSet):
+class TrackViewSet(CustomModelViewSet):
     queryset = Track.objects.all()
     serializer_class = TrackSerializer
     permission_classes = [permissions.IsAuthenticatedOrReadOnly]
