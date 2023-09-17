a13bfcf4c6da993fc3682c28a356d33960511f24
dmytro.malikin <dmytro.malikin@gmail.com>
2023-09-17 21:42:43 +0300

    Add view playlist tracks

--- client/src/components/rows/PlaylistRow.js
+++ client/src/components/rows/PlaylistRow.js
@@ -1,15 +1,41 @@
-import React from "react";
+import React, { useEffect, useMemo, useState } from "react";
 import styles from "./PlaylistRow.module.css";
+import TrackRow from "./TrackRow";
 
 function PlaylistRow({ playlist, onDelete }) {
+  const [isPlaylistOpen, setIsPlaylistOpen] = useState(false);
+  const [tracksData, setTracksData] = useState({ isLoaded: false, data: [] });
   const handleDeletePlaylist = () => onDelete(playlist.id);
+  const handleTogglePlaylist = () => setIsPlaylistOpen((prev) => !prev);
+
+  useEffect(() => {
+    if (!isPlaylistOpen || tracksData.isLoaded) return;
+    fetch(`http://0.0.0.0:8000/playlists/${playlist.id}/`, { mode: "cors" })
+      .then((res) => res.json())
+      .then((data) => {
+        setTracksData({ isLoaded: true, data: data.tracks });
+      });
+  }, [isPlaylistOpen, tracksData.isLoaded]);
+
+  const playlistContent = useMemo(() => {
+    if (!isPlaylistOpen) return null;
+    if (!tracksData.isLoaded) return <span>Loading...</span>;
+    if (!tracksData.data.length) return <span>No tracks yet</span>;
+    return tracksData.data.map((track, idx) => (
+      <TrackRow key={idx} track={track} />
+    ));
+  }, [tracksData, isPlaylistOpen]);
+
   return (
-    <div className={styles.playlistRow}>
-      <div className={styles.playlistTitle}>{playlist.title}</div>
-      <button className={styles.deleteButton} onClick={handleDeletePlaylist}>
-        Delete Playlist
-      </button>
-    </div>
+    <>
+      <div className={styles.playlistRow} onClick={handleTogglePlaylist}>
+        <div className={styles.playlistTitle}>{playlist.title}</div>
+        <button className={styles.deleteButton} onClick={handleDeletePlaylist}>
+          Delete Playlist
+        </button>
+      </div>
+      {playlistContent}
+    </>
   );
 }
 
