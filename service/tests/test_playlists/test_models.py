6e8e97d2c2334b949a11c0ca075c60b43bccabd7
dmytro.malikin <dmytro.malikin@gmail.com>
2023-09-18 12:34:22 +0300

    Update models tests with several tracks to get order

--- service/tests/test_playlists/test_models.py
+++ service/tests/test_playlists/test_models.py
@@ -29,38 +29,39 @@ class PlaylistModelTestCase(TestCase):
 
 class PlaylistTrackModelTestCase(TestCase):
     def setUp(self):
-        self.track = Track.objects.create(id="track_1", title="Track 1")
+        self.track1 = Track.objects.create(id="track_1", title="Track 1")
+        self.track2 = Track.objects.create(id="track_2", title="Track 2")
         self.playlist = Playlist.objects.create(title="My Playlist")
 
     def test_create_playlist_track(self):
         playlist_track = PlaylistTrack.objects.create(
-            track=self.track,
+            track=self.track1,
             playlist=self.playlist,
             order=1,
         )
-        self.assertEqual(playlist_track.track, self.track)
+        self.assertEqual(playlist_track.track, self.track1)
         self.assertEqual(playlist_track.playlist, self.playlist)
 
     def test_unique_together_constraint(self):
-        PlaylistTrack.objects.create(track=self.track, playlist=self.playlist, order=1)
+        PlaylistTrack.objects.create(track=self.track1, playlist=self.playlist, order=1)
         with self.assertRaises(IntegrityError):
             PlaylistTrack.objects.create(
-                track=self.track,
+                track=self.track1,
                 playlist=self.playlist,
                 order=1,
             )
 
     def test_get_latest_by_order(self):
         playlist_track1 = PlaylistTrack.objects.create(
-            track=self.track,
-            playlist=self.playlist,
-            order=1,
-        )
-        playlist_track2 = PlaylistTrack.objects.create(
-            track=self.track,
+            track=self.track1,
             playlist=self.playlist,
             order=2,
         )
+        playlist_track2 = PlaylistTrack.objects.create(
+            track=self.track2,
+            playlist=self.playlist,
+            order=1,
+        )
 
         latest_track = PlaylistTrack.objects.latest("order")
-        self.assertEqual(latest_track, playlist_track2)
+        self.assertEqual(latest_track, playlist_track1)
