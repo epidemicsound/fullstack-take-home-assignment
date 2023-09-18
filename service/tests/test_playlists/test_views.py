f458fda4d221abeb4fa9ba581fede0e70549b440
dmytro.malikin <dmytro.malikin@gmail.com>
2023-09-18 12:31:09 +0300

    Add not found cases in views tests

--- service/tests/test_playlists/test_views.py
+++ service/tests/test_playlists/test_views.py
@@ -29,6 +29,11 @@ class PlaylistViewSetTestCase(TestCase):
         response = self.client.get(url)
         self.assertEqual(response.status_code, status.HTTP_200_OK)
 
+    def test_retrieve_playlist_not_found(self):
+        url = reverse("playlist-detail", args=["random"])
+        response = self.client.get(url)
+        self.assertEqual(response.status_code, status.HTTP_404_NOT_FOUND)
+
     def test_create_playlist(self):
         data = {"title": "New Playlist"}
         url = reverse("playlist-list")
@@ -45,6 +50,12 @@ class PlaylistViewSetTestCase(TestCase):
         self.playlist.refresh_from_db()
         self.assertEqual(self.playlist.title, "Updated Playlist")
 
+    def test_update_playlist_not_found(self):
+        data = {"title": "Updated Playlist"}
+        url = reverse("playlist-detail", args=["random"])
+        response = self.client.put(url, data, format="json")
+        self.assertEqual(response.status_code, status.HTTP_404_NOT_FOUND)
+
     def test_list_playlist_tracks(self):
         url = reverse("playlist-playlist-tracks", args=[self.playlist.id])
         response = self.client.get(url)
@@ -56,12 +67,23 @@ class PlaylistViewSetTestCase(TestCase):
         track = data[0]
         self.assertEqual(track["id"], self.playlist_track1.track_id)
 
+    def test_list_playlist_tracks_not_found(self):
+        url = reverse("playlist-playlist-tracks", args=["random"])
+        response = self.client.get(url)
+        self.assertEqual(response.status_code, status.HTTP_404_NOT_FOUND)
+
     def test_add_track_to_playlist(self):
         data = [{"track_id": self.track2.id}]
         url = reverse("playlist-playlist-tracks", args=[self.playlist.id])
         response = self.client.post(url, data, format="json")
         self.assertEqual(response.status_code, status.HTTP_200_OK)
         self.assertEqual(PlaylistTrack.objects.count(), 2)
+
+    def test_add_track_to_playlist_not_found(self):
+        data = [{"track_id": self.track2.id}]
+        url = reverse("playlist-playlist-tracks", args=["random"])
+        response = self.client.post(url, data, format="json")
+        self.assertEqual(response.status_code, status.HTTP_404_NOT_FOUND)
 
     def test_delete_track_from_playlist(self):
         url = reverse(
@@ -71,3 +93,18 @@ class PlaylistViewSetTestCase(TestCase):
         response = self.client.delete(url)
         self.assertEqual(response.status_code, status.HTTP_204_NO_CONTENT)
         self.assertEqual(PlaylistTrack.objects.count(), 0)
+
+    def test_delete_track_from_playlist_not_found(self):
+        url = reverse(
+            "playlist-delete-track",
+            args=[self.playlist.id, "random"],
+        )
+        response = self.client.delete(url)
+        self.assertEqual(response.status_code, status.HTTP_404_NOT_FOUND)
+
+        url = reverse(
+            "playlist-delete-track",
+            args=[42, self.playlist_track1.track.id],
+        )
+        response = self.client.delete(url)
+        self.assertEqual(response.status_code, status.HTTP_404_NOT_FOUND)
