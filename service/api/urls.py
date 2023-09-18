4896f0918f74d463026414a1f17958db4f625a27
dmytro.malikin <dmytro.malikin@gmail.com>
2023-09-18 12:08:20 +0300

    Include playlists routes

--- service/api/urls.py
+++ service/api/urls.py
@@ -5,6 +5,7 @@ from . import views
 
 router = routers.DefaultRouter()
 router.register(r"tracks", views.TrackViewSet)
+router.register(r"playlists", views.PlaylistViewSet)
 
 urlpatterns = [
     path("", include(router.urls)),
