676995d869d219ac83f5f83582d0b2688975a70b
Edrich Chua <edrichhans@gmail.com>
2023-09-21 16:30:45 +0800

    feat(client): Inject tracks view into playlist

--- client/src/views/Playlists.js
+++ client/src/views/Playlists.js
@@ -1,9 +1,11 @@
 import React, { useEffect, useState } from 'react';
 import styles from './Playlists.module.css'
 import { formatDate } from '../util/formatter';
+import Tracks from './Tracks';
 
 const Playlists = () => {
   const [playlists, setPlaylists] = useState([]);
+  const [selectedPlaylist, setSelectedPlaylist] = useState(null);
 
   useEffect(() => {
     fetch("http://0.0.0.0:8000/playlists/", { mode: "cors" })
@@ -11,18 +13,23 @@ const Playlists = () => {
       .then((data) => setPlaylists(data));
   }, []);
 
-    return (
-      <div className={styles.playlistContainer}>
-        <ul className={styles.playlistMenu}>
-          {playlists.map((playlist, ix) => (
-            <div key={`playlist-${ix}`} className={styles.playlistItem__container}>
-              <li className={styles.playlistItem__title}>{playlist.name}</li>
-              <span className={styles.playlistItem__subtitle}>{formatDate(playlist.created_at)}</span>
-            </div>
-          ))}
-        </ul>
-      </div>
-    )
+  const handleSelectPlaylist = (playlist) => {
+    setSelectedPlaylist(playlist);
+  }
+
+  return (
+    <div className={styles.playlistContainer}>
+      <ul className={styles.playlistMenu}>
+        {playlists.map((playlist, ix) => (
+          <div key={`playlist-${ix}`} className={styles.playlistItem__container} onClick={() => handleSelectPlaylist(playlist)}>
+            <li className={styles.playlistItem__title}>{playlist.name}</li>
+            <span className={styles.playlistItem__subtitle}>{formatDate(playlist.created_at)}</span>
+          </div>
+        ))}
+      </ul>
+      {selectedPlaylist && <Tracks className={styles.playlistTracks} tracks={selectedPlaylist.tracks} />}
+    </div>
+  )
 }
 
 export default Playlists;
