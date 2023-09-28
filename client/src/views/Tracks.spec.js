88d02c0a7db03589c11f7085a950c39cb9977551
Edrich Chua <edrichhans@gmail.com>
2023-09-28 09:01:35 +0800

    test(client): assert track is added to store

--- client/src/views/Tracks.spec.js
+++ client/src/views/Tracks.spec.js
@@ -1,10 +1,19 @@
-import { screen, render } from '@testing-library/react';
+import { screen, render, waitFor } from '@testing-library/react';
+import userEvent from '@testing-library/user-event';
 import Tracks from './Tracks';
 import store from '../store/store';
 import { Provider } from 'react-redux';
 import { tracks } from '../mocks/tracks';
+import { setPlaylists } from '../store/actions';
+import { playlists } from '../mocks/playlists';
 
 describe('Tracks', () => {
+  const user = userEvent.setup();
+
+  beforeAll(async () => {
+    await store.dispatch(setPlaylists()).unwrap();
+  });
+
   it('renders the list of tracks correctly', () => {
     render(
       <Provider store={store}>
@@ -49,15 +58,60 @@ describe('Tracks', () => {
         'select-playlist-dropdown'
       );
       expect(selectPlaylistDropdown).toBeInTheDocument();
+
       expect(selectPlaylistDropdown).toHaveTextContent(
         '-- select an option --'
+      );
+      playlists.forEach(playlist => {
+        expect(selectPlaylistDropdown).toHaveTextContent(playlist.name);
+      });
+    });
+
+    it('add track to playlist store when a playlist is selected', async () => {
+      render(
+        <Provider store={store}>
+          <Tracks tracks={tracks} />
+        </Provider>
+      );
+
+      const track = screen.getByTestId('track-0');
+      track.dispatchEvent(new MouseEvent('contextmenu', { bubbles: true }));
+
+      const addToPlaylist = await screen.findByText('Add to playlist');
+      addToPlaylist.dispatchEvent(new MouseEvent('click', { bubbles: true }));
+
+      const selectPlaylistDropdown = await screen.findByTestId(
+        'select-playlist-dropdown'
+      );
+      selectPlaylistDropdown.dispatchEvent(
+        new MouseEvent('click', { bubbles: true })
+      );
+
+      await userEvent.selectOptions(
+        selectPlaylistDropdown,
+        screen.getByRole('option', {
+          name: playlists[0].name
+        })
+      );
+
+      expect(
+        screen.getByRole('option', { name: playlists[0].name }).selected
+      ).toBeTruthy();
+
+      const okayButton = screen.getByRole('button', { name: 'Okay' });
+      expect(okayButton).toBeInTheDocument();
+      expect(okayButton).not.toBeDisabled();
+      // okayButton.dispatchEvent(new MouseEvent('click', { bubbles: true }));
+      user.click(okayButton);
+
+      await waitFor(() =>
+        expect(store.getState().player.playlists[0].tracks).toEqual(
+          expect.arrayContaining([tracks[0]])
+        )
       );
     });
 
     // TODO:
-    // add msw to mock API calls, then create tests to test the following:
-    // - setup the test suite to populate the store with playlists
-    // - when a playlist is selected, the track is added to the playlist store
     // - when a playlist is selected, an API call was made to add the track to the playlist
     // - when a playlist is selected, the modal is closed
     // - when a playlist is selected, the context menu is closed
