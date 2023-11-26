f2778850f2a0f3df4012e2ab8402911630fbbb7c
Amirmasoud Ramezani <amirmasoud.rz@hotmail.com>
2023-11-26 13:48:39 +0100

    feat: route registration for PlaylistViewSet

--- service/api/urls.py
+++ service/api/urls.py
@@ -1,11 +1,11 @@
 from django.urls import include, path
 from rest_framework import routers
-
 from . import views
 
 router = routers.DefaultRouter()
 router.register(r"tracks", views.TrackViewSet)
+router.register(r'playlists', views.PlaylistViewSet, basename='playlist')
 
 urlpatterns = [
-    path("", include(router.urls)),
+    path("", include(router.urls))
 ]
